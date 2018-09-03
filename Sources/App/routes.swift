a35e8b41ccd2124f7a27cd9d62753b43b6863b5d
gaopin1534 <gaoxin.xixxix@gmail.com>
2018-09-03 14:28:23 -0400

    add json get example

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,10 +1,20 @@
 import Vapor
 
 /// Register your application's routes here.
+
+struct JSONExample: Content {
+    let name: String
+    let birthday: Date
+}
 public func routes(_ router: Router) throws {
     // Basic "Hello, world!" example
     router.get("hello") { req in
         return "Hello, world!"
+    }
+    
+    router.get("json") {
+         req -> JSONExample in
+        return JSONExample(name: "hoge", birthday: Date())
     }
 
     // Example of configuring a controller
